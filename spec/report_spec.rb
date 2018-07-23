2fb0e940d481981fda241f4b000578a954890541
scossar <scossar3@gmail.com>
2018-07-23 16:56:34 -0700

    Add report_spec

---
+++ spec/report_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe Report do
+  describe 'staff notes report' do
+    let(:report) { Report.find('staff_notes', start_date: 1.month.ago.beginning_of_day, end_date: Date.today.end_of_day) }
+    let(:user) { Fabricate(:user) }
+    let(:mod) { Fabricate(:admin) }
+    let(:first_topic) { Fabricate(:topic) }
+    let(:second_topic) { Fabricate(:topic) }
+
+    context "when there are no staff notes" do
+      before do
+        SiteSetting.staff_notes_enabled = true
+      end
+
+      it "should return a report with no data" do
+        expect(report.data).to be_blank
+      end
+    end
+
+    context "when there are staff notes" do
+      before do
+        SiteSetting.staff_notes_enabled = true
+        UserWarning.create(topic_id: first_topic.id, user_id: user.id, created_by_id: mod.id)
+        UserWarning.create(topic_id: second_topic.id, user_id: user.id, created_by_id: mod.id)
+      end
+
+      it "should return the most recent note for the user" do
+        expect(report.data[0][:note]).to match(/#{second_topic.title}/)
+      end
+
+      it "should return the user URLs" do
+        expect(report.data[0][:user_url]).to eq("/admin/users/#{user.id}/#{user.username_lower}")
+        expect(report.data[0][:moderator_url]).to eq("/admin/users/-1/system")
+      end
+    end
+  end
+end
