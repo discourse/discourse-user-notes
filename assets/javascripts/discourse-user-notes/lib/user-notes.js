e9f65664742e0e1b0877f1ce3ed961f0d636af7d
Kelvin Tan <kelv@discourse.org>
2023-11-23 12:08:29 +0800

    DEV: refactor to use getOwnerWithFallback instead of getOwner

--- assets/javascripts/discourse-user-notes/lib/user-notes.js
+++ assets/javascripts/discourse-user-notes/lib/user-notes.js
@@ -1,8 +1,8 @@
 import UserNotesModal from "../../discourse/components/modal/user-notes";
-import { getOwner } from "discourse-common/lib/get-owner";
+import { getOwnerWithFallback } from "discourse-common/lib/get-owner";
 
 export function showUserNotes(store, userId, callback, opts) {
-  const modal = getOwner(this).lookup("service:modal");
+  const modal = getOwnerWithFallback(this).lookup("service:modal");
   opts = opts || {};
 
   return store.find("user-note", { user_id: userId }).then((model) => {
